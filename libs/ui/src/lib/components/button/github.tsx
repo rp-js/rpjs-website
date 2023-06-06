8d16f553e1b61428caff119adb66bbda0bd6f2c6
Italo Covas <italocovas@gmail.com>
2023-06-06 19:37:01 -0300

    refactor: ui of login page

---
+++ libs/ui/src/lib/components/button/github.tsx
@@ -0,0 +1,10 @@
+import { AiFillGithub } from 'react-icons/ai';
+
+export function GithubButton() {
+  return (
+    <button className="px-4 py-2 w-4/5 border bg-[#333333] flex items-center gap-2 border-slate-200 rounded-lg text-white font-bold hover:border-slate-400 hover:shadow transition duration-150">
+      <AiFillGithub className="mr-2 flex-shrink-0" size="20px" fill="white" />
+      <span className="block text-left">Entre com o GitHub</span>
+    </button>
+  );
+}
--- libs/ui/src/lib/components/login-modal.tsx
+++ libs/ui/src/lib/components/login-modal.tsx
@@ -2,14 +2,18 @@
 
 import Image from 'next/image';
 import { Modal } from 'antd';
-import { AiFillGithub } from 'react-icons/ai';
-import { FaGoogle } from 'react-icons/fa';
 import { Dispatch, SetStateAction } from 'react';
-import { Button } from './button';
+import { GithubButton } from './button/github';
+import { GoogleButton } from './button/google';
 
 interface LoginModalProps {
   isOpen: boolean;
   setIsOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+interface ImageProps {
+  source: string;
+  altText: string;
 }
 
 export default function LoginModal({ isOpen, setIsOpen }: LoginModalProps) {
@@ -18,30 +22,64 @@ export default function LoginModal({ isOpen, setIsOpen }: LoginModalProps) {
     document.body.style.overflow = 'auto';
   }
 
+  function generateRandomImage() {
+    const images = [
+      {
+        source: '/login.svg',
+        altText: 'Garota acessando sua conta em um celular.',
+      },
+      {
+        source: '/login2.svg',
+        altText: 'Mulher acessando sua conta em um celular.',
+      },
+      {
+        source: '/login3.svg',
+        altText: 'Garoto colocando uma chave dentro de um cadeado, simbolizando a senha.',
+      },
+      {
+        source: '/login4.svg',
+        altText: 'Garota acessando sua conta em um celular.',
+      },
+      {
+        source: '/login5.svg',
+        altText: 'Garoto e garota acessando suas contas em um celular.',
+      },
+    ];
+
+    const previousImages: ImageProps[] = [];
+
+    const availableImages = images.filter((image) => !previousImages.includes(image));
+
+    if (availableImages.length === 0) {
+      return images[Math.floor(Math.random() * 5)];
+    }
+
+    const randomIndex = Math.floor(Math.random() * availableImages.length);
+    const randomImage = availableImages[randomIndex];
+
+    previousImages.push(randomImage);
+
+    return randomImage;
+  }
+
+  const randomImage = generateRandomImage();
+  console.log('iiiii');
+
   return (
     <Modal centered okType="default" open={isOpen} onOk={closeModal} onCancel={closeModal} footer={null}>
-      <Image
-        className="flex-shrink-0 mx-auto mb-4"
-        src="/logo.svg"
-        alt="logo amarelo escrito r p j s"
-        width={44}
-        height={44}
-      />
-      <h1 className=" text-center font-bold text-xl mb-4">Entre no RP.js!</h1>
-      <p className="text-center   text-gray-500  text-lg line  leading-5">
-        Entre em nosso site, tenha acesso ao seu perfil, inscreva-se no próximo evento e muito mais com apenas alguns
-        cliques.
-      </p>
-      <div className="flex flex-col gap-3 mt-4">
-        <Button type="button" button="social" onClick={closeModal}>
-          <FaGoogle className="mr-2" size={20} />
-          Entrar com o Google
-        </Button>
-
-        <Button type="button" button="social" onClick={closeModal}>
-          <AiFillGithub className="mr-2" size={20} />
-          Entrar com o GitHub
-        </Button>
+      <div className=" py-5">
+        <h1 className="font-bold  text-3xl mb-8 text-center">Entre no RP.Js!</h1>
+        <Image
+          className="flex-shrink-0 mx-auto mb-4"
+          src={randomImage.source}
+          alt={randomImage.altText}
+          width={300}
+          height={300}
+        />
+        <div className="flex justify-center items-center w-full flex-col gap-3 mt-8">
+          <GoogleButton />
+          <GithubButton />
+        </div>
       </div>
     </Modal>
   );
