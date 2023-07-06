1cb412ffd46c0c6d50e1198753584081900e8dc2
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2023-07-06 12:59:53 -0300

    feat: add google provider for next-auth

--- apps/rpjs-site/environment.d.ts
+++ apps/rpjs-site/environment.d.ts
@@ -2,9 +2,11 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       DATABASE_URL: string;
-      GITHUB_SECRET: string;
-      GITHUB_ID: string;
       NEXTAUTH_URL: string;
+      GITHUB_ID: string;
+      GITHUB_SECRET: string;
+      GOOGLE_ID: string;
+      GOOGLE_SECRET: string;
     }
   }
 }
